5e3ccca0a78ce619b508db0930017b553ebc8c61
PresidentCourtnee <52513651+PresidentCourtnee@users.noreply.github.com>
2020-06-11 21:13:12 -0600

    Changes putting info into Customer data

---
+++ CustomerData/CustomerDBReader.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CustomerData
+{
+    class CustomerDBReader
+    {
+        private MySqlConnection con;
+        public CustomerDBReader(MySqlConnection con)
+        {
+            this.con = con;
+        }
+
+        public void readAllData()
+        {
+            string sql = "SELECT * FROM customer";
+            using var cmd = new MySqlCommand(sql, con);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+
+            Console.WriteLine("\t|\t  Number |\t  Name  \t\t|\t Direction  |");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"\t|\t{reader.GetInt32(0)}\t|\t Congratulations: {reader.GetString(1)}\t|\t From: {reader.GetString(2)}");
+            }
+        }
+    }
+}
--- CustomerData/Program.cs
+++ CustomerData/Program.cs
@@ -15,21 +15,9 @@ namespace CustomerData
 
             Console.WriteLine($"MySQL version:   {con.ServerVersion}");
 
-            var stm = "SELECT VERSION()";
-            var cmd = new MySqlCommand(stm, con);
-
-            var verson = cmd.ExecuteScalar().ToString();
-            Console.WriteLine($"MySQL version: {verson}");
-
-            insertData(con);
-                   
-        }
-
-        private static void insertData(MySqlConnection con)
-        {
-            var stm = "INSERT INTO cars (name, price) VALUES ('Limo', '65465189616')";
-            var cmd = new MySqlCommand(stm, con);
-            cmd.ExecuteNonQuery();
+            CustomerDBReader customerDBReader = new CustomerDBReader(con);
+            customerDBReader.readAllData();
+                               
         }
     }
 }
